12deb47705bd9c225c4fab596160cd909b284095
GreenDog3 <greendognc@gmail.com>
2025-09-15 10:59:48 -0500

    Adding Project

---
+++ Source/NCarawayGPE230/NCarawayGPE230.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "NCarawayGPE230.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, NCarawayGPE230, "NCarawayGPE230" );
---
+++ Source/NCarawayGPE230/NCarawayGPE230GameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "NCarawayGPE230GameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class NCARAWAYGPE230_API ANCarawayGPE230GameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
